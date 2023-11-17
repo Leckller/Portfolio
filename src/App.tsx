dd8711869c2200ca1bf7d62522d4afef0f139812
Leckller <7leckler@gmail.com>
2023-11-16 23:47:23 -0300

    feat: iniciando projeto

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Route, Routes } from 'react-router-dom';
+
+function App() {
+  return (
+    <Routes>
+      <Route>
+        oi
+      </Route>
+    </Routes>
+  );
+}
+
+export default App;
