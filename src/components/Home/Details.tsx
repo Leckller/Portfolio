f538ff66903575a12a2c214307a26545aa3073b2
Leckller <7leckler@gmail.com>
2023-12-13 12:28:16 -0300

    fix: style bugs; feat: new animations

--- src/components/Home/Details.tsx
+++ src/components/Home/Details.tsx
@@ -1,13 +1,13 @@
-import { FaLongArrowAltDown } from 'react-icons/fa';
+import { FaArrowAltCircleDown, FaLongArrowAltDown } from 'react-icons/fa';
 import useWriteText from '../../hooks/useWriteText';
-import { ANC, LabelAnc } from '../../styles';
+import { ANC, ArrowDown, LabelAnc } from '../../styles';
 import { projetos } from '../../utils/projetos';
 import { tecnologias } from '../../utils/tecnologias';
 import Card from '../Projetos/Card';
 import CardDetails from './CardDetails';
 
 function Details({ detail = false }: { detail?: boolean }) {
-  const title = detail ? 'Especializado nas Linguagens' : 'Projetos';
+  const title = detail ? 'Especializado nas Tecnologias' : 'Projetos';
   const write = useWriteText(title, 20);
   const writeimages = useWriteText(tecnologias, 300);
 
@@ -19,14 +19,21 @@ function Details({ detail = false }: { detail?: boolean }) {
       ${detail ? 'text-black' : 'text-white'} text-xl font-medium` }
     >
 
-      <h1 className="overflow-hidden">
+      <h1
+        className={ `overflow-hidden h-16 justify-start
+        relative w-96 flex flex-row items-center gap-5
+        ${detail ? '' : 'pl-5'}` }
+      >
         {`${title.slice(0, write)} ${write >= title.length ? '' : '|'}`}
+        <ArrowDown detail={ detail }>
+          <FaArrowAltCircleDown className={ `${detail ? '' : '-rotate-45'}` } />
+        </ArrowDown>
       </h1>
       {write >= title.length ? (
         <div
-          className="flex flex-row flex-wrap md:w-1/2
-        items-center justify-around gap-1 mb-16 md:border-r-2
-        "
+          className={ `flex flex-row flex-wrap
+          items-center justify-around gap-1 mb-16 
+          ${detail ? 'md:border-r-2 md:w-1/2' : 'overflow-y-hidden w-full'}` }
         >
           {detail ? (
             tecnologias && tecnologias.slice(0, writeimages).map((tec) => (
