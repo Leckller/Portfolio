6eee67f2cde41093961deb78ac5c572ec652f4eb
Leckller <7leckler@gmail.com>
2023-12-14 12:11:17 -0300

    feat: melhoria de performace

--- src/components/Home/DetailsCursos.tsx
+++ src/components/Home/DetailsCursos.tsx
@@ -9,10 +9,8 @@ function DetailsCursos() {
     text3: 'Cursando Engenharia de Software na',
     text4: 'Em constante aprendizado com novas tecnologias como',
   };
-  const t1 = useWriteText(textos.text, 20);
-  const t2 = useWriteText(textos.text2, 20);
-  const t3 = useWriteText(textos.text3, 20);
-  const t4 = useWriteText(textos.text4, 20);
+  const textosLength = textos.text + textos.text2 + textos.text3 + textos.text4;
+  const t1 = useWriteText(textosLength, 20);
   return (
     <div className="flex flex-col w-full pl-5 md:w-1/2 ">
       <h1
@@ -28,25 +26,25 @@ function DetailsCursos() {
       </h1>
       <ul className="flex flex-col gap-3">
         <li className="border-b pb-5 pt-5 max-w-3xl">
-          {`${textos.text2.slice(0, t2)} ${t2 >= textos.text2.length ? '' : '|'}`}
+          {`${textos.text2.slice(0, t1)} ${t1 >= textos.text2.length ? '' : '|'}`}
           {' '}
-          {t2 >= textos.text2.length && (
+          {t1 >= textos.text2.length && (
             <span className="text-green-500 animate-surgir">Trybe</span>
           )}
         </li>
         <li className="border-b pb-5 pt-5 max-w-3xl">
-          {`${textos.text3.slice(0, t3)} ${t3 >= textos.text3.length ? '' : '|'}`}
+          {`${textos.text3.slice(0, t1)} ${t1 >= textos.text3.length ? '' : '|'}`}
           {' '}
-          {t3 >= textos.text3.length && (
+          {t1 >= textos.text3.length && (
             <span className="text-blue-500 animate-surgir">
               Estácio
             </span>
           )}
         </li>
         <li className="border-b pb-5 pt-5 max-w-3xl">
-          {`${textos.text4.slice(0, t4)} ${t4 >= textos.text4.length ? '' : '|'}`}
+          {`${textos.text4.slice(0, t1)} ${t1 >= textos.text4.length ? '' : '|'}`}
           {' '}
-          {t4 >= textos.text4.length && (
+          {t1 >= textos.text4.length && (
             <span className="text-red-500 animate-surgir">
               Inteligência artificial
             </span>
