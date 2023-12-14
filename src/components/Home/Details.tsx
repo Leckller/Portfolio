eb82e4defc033aeab088e489803a59f6d893c829
Leckller <7leckler@gmail.com>
2023-12-14 10:16:21 -0300

    feat: cursos!

--- src/components/Home/Details.tsx
+++ src/components/Home/Details.tsx
@@ -1,16 +1,11 @@
-import { FaArrowAltCircleDown, FaLongArrowAltDown } from 'react-icons/fa';
-import useWriteText from '../../hooks/useWriteText';
-import { ANC, ArrowDown, LabelAnc } from '../../styles';
-import { projetos } from '../../utils/projetos';
+/* eslint-disable complexity */
+/* eslint-disable react/jsx-max-depth */
 import { tecnologias } from '../../utils/tecnologias';
-import Card from '../Projetos/Card';
-import CardDetails from './CardDetails';
+import DetailsCursos from './DetailsCursos';
+import DetailsTecnologias from './DetailsTecnologias';
+import DetailsAnc from './DetailsAnc';
 
 function Details({ detail = false }: { detail?: boolean }) {
-  const title = detail ? 'Especializado nas Tecnologias' : 'Projetos';
-  const write = useWriteText(title, 20);
-  const writeimages = useWriteText(tecnologias, 300);
-
   return (
     <section
       id={ detail ? '' : 'details' }
@@ -19,71 +14,13 @@ function Details({ detail = false }: { detail?: boolean }) {
       ${detail ? 'text-black' : 'text-white'} text-xl font-medium` }
     >
 
-      <h1
-        className={ `overflow-hidden h-16 justify-start
-        relative w-96 flex flex-row items-center gap-5
-        ${detail ? '' : 'pl-5'}` }
-      >
-        {`${title.slice(0, write)} ${write >= title.length ? '' : '|'}`}
-        <ArrowDown detail={ detail }>
-          <FaArrowAltCircleDown className={ `${detail ? '' : '-rotate-45'}` } />
-        </ArrowDown>
-      </h1>
-      {write >= title.length ? (
-        <div
-          className={ `flex flex-row flex-wrap
-          items-center justify-around gap-1 mb-16 
-          ${detail ? 'md:border-r-2 md:w-1/2' : 'overflow-y-hidden w-full'}` }
-        >
-          {detail ? (
-            tecnologias && tecnologias.slice(0, writeimages).map((tec) => (
-              <CardDetails
-                img={ tec.img }
-                title={ tec.title }
-                key={ tec.title }
-              />
-            ))
+      <div className="flex-row flex w-full">
+        <DetailsTecnologias detail={ detail } />
+        {detail && <DetailsCursos />}
+      </div>
 
-          ) : (
-
-            projetos && projetos.map((proj) => (
-              <Card
-                url={ proj.url }
-                image={ proj.img }
-                title={ proj.title }
-                key={ proj.title }
-              />
-            ))
-          )}
-
-        </div>
-      ) : (
-        <div />
-      )}
-      {writeimages >= tecnologias.length && (
-        <LabelAnc
-          htmlFor="more"
-          className="flex flex-col
-              w-full h-1/4 justify-center
-              items-center gap-2 pb-10"
-        >
-          <div className="text-center text-black">
-            <h3>
-              Ficou interessado?
-            </h3>
-            <h3>
-              clique aqui para ver meus projetos
-            </h3>
-          </div>
-          <ANC
-            href="#details"
-            className="w-10 h-10 rounded-full border border-black flex items-center
-                justify-center text-black"
-          >
-            <FaLongArrowAltDown />
-          </ANC>
-        </LabelAnc>
-
+      {tecnologias.length <= 100 && (
+        <DetailsAnc />
       )}
     </section>
 
---
+++ src/components/Home/DetailsCursos.tsx
@@ -0,0 +1,36 @@
+function DetailsCursos() {
+  return (
+    <div className="flex flex-col w-1/2 ">
+      <h1
+        className="w-96 overflow-hidden h-16 justify-start relative
+    flex flex-row items-center gap-5"
+      >
+        Cursos
+
+      </h1>
+      <div className="p-5">
+        <h2>
+          Desenvolvimento Web FullStack na
+          {' '}
+          <span className="text-green-500">Trybe</span>
+        </h2>
+        <h2>
+          Cursando Engenharia de Software na
+          {' '}
+          <span className="text-blue-500">
+            Estácio
+          </span>
+        </h2>
+        <h2>
+          Em constante aprendizado com novas tecnologias como
+          {' '}
+          <span className="text-red-500">
+            Inteligência artificial
+          </span>
+        </h2>
+      </div>
+    </div>
+  );
+}
+
+export default DetailsCursos;
