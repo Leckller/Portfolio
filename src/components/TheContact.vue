63797e8dbeca9b0f36b027402b5b74dc76379192
Gusttavo Ruy <7leckler@gmail.com>
2025-08-17 14:03:03 -0300

    feat: componente de contato

---
+++ src/components/TheContact.vue
@@ -0,0 +1,65 @@
+<template>
+  <section id="contato">
+    <h3 class="title">Entre em Contato</h3>
+
+    <div>
+      <div class="details">
+        <h4 class="sub-title">Vamos conversar!</h4>
+        <p class="text">
+          Estou sempre interessado em novos projetos e oportunidades. Se você
+          tem uma ideia ou precisa de ajuda com um projeto, vamos conversar!
+        </p>
+        <p class="contato-info">
+          <i class="fa-solid fa-envelope"></i>
+          gusttavoruynascimento@gmail.com
+        </p>
+        <p class="contato-info">+55 (21) 984255938</p>
+        <p class="contato-info">
+          <i class="fa-brands fa-linkedin"></i>
+          www.linkedin.com/in/gusttavoruy
+        </p>
+        <p class="contato-info">
+          <i class="fa-brands fa-github"></i>
+          https://github.com/Leckller
+        </p>
+      </div>
+
+      <form class="form">
+        <label class="label">
+          Nome
+          <input class="input" type="text" />
+        </label>
+        <label class="label">
+          Email
+          <input class="input" type="text" />
+        </label>
+        <label class="label">
+          Mensagem
+          <input class="input" type="text" />
+        </label>
+        <button class="form-button">Enviar Mensagem</button>
+      </form>
+    </div>
+  </section>
+</template>
+
+<style scoped>
+#contato {
+}
+.title {
+}
+.details {
+}
+.sub-title {
+}
+.text {
+}
+.contato-info {
+}
+.form {
+}
+.label {
+}
+.input {
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,12 +5,14 @@
     <TheAbilities />
     <TheProjects />
     <TheExperience />
+    <TheContact />
   </TheLayout>
 </template>
 
 <script setup>
 import AboutMe from "@/components/AboutMe.vue";
 import TheAbilities from "@/components/TheAbilities.vue";
+import TheContact from "@/components/TheContact.vue";
 import TheExperience from "@/components/TheExperience.vue";
 import TheLayout from "@/components/TheLayout.vue";
 import ThePresentation from "@/components/ThePresentation.vue";
